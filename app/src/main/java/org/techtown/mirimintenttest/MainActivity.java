ac4b589b828d4fdf55acbe92d32c61edd03fbc45
dayeon <dayeon2399@naver.com>
2020-11-26 19:35:12 +0900

    dayeon's commit - xml완료, MainActivity 작업 중

--- app/src/main/java/org/techtown/mirimintenttest/MainActivity.java
+++ app/src/main/java/org/techtown/mirimintenttest/MainActivity.java
@@ -2,13 +2,30 @@ package org.techtown.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_compute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        btn_compute=findViewById(R.id.go_computation);
+        btn_compute.setOnClickListener(goButtonClickListener);
     }
+    View.OnClickListener goButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            //intent.putExtra();
+        }
+    };
 }
--- app/src/main/java/org/techtown/mirimintenttest/ResultActivity.java
+++ app/src/main/java/org/techtown/mirimintenttest/ResultActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_result extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
