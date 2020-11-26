3619464446c39197c3f8905277738644b4c8b599
dayeon <dayeon2399@naver.com>
2020-11-26 19:51:14 +0900

    dayeon's commit - xml완료, MainActivity 완료 ResultActivity 작업 중

--- app/src/main/java/org/techtown/mirimintenttest/MainActivity.java
+++ app/src/main/java/org/techtown/mirimintenttest/MainActivity.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_compute;
+    EditText edit1,edit2;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edit1=findViewById(R.id.edit_1);
+        edit2=findViewById(R.id.edit_2);
 
+        radioGroup=findViewById(R.id.arithmetic);
 
         btn_compute=findViewById(R.id.go_computation);
         btn_compute.setOnClickListener(goButtonClickListener);
@@ -25,7 +32,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-            //intent.putExtra();
+            intent.putExtra("num1",edit1.getText().toString());
+            intent.putExtra("num2",edit2.getText().toString());
+
+            switch (radioGroup.getCheckedRadioButtonId()){
+                case R.id.check_1:// +
+                    intent.putExtra("arithmetic","+");
+                    break;
+                case R.id.check_2:// -
+                    intent.putExtra("arithmetic","-");
+                    break;
+                case R.id.check_3:// *
+                    intent.putExtra("arithmetic","*");
+                    break;
+                case R.id.check_4:// /
+                    intent.putExtra("arithmetic","/");
+            }
+
         }
     };
 }
--- app/src/main/java/org/techtown/mirimintenttest/ResultActivity.java
+++ app/src/main/java/org/techtown/mirimintenttest/ResultActivity.java
@@ -3,12 +3,16 @@ package org.techtown.mirimintenttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        textView=findViewById(R.id.result);
+        //textView.setText();
     }
 }
