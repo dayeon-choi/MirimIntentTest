0fcb0a36335dd81180e0b10056351a4fee0f064c
dayeon <dayeon2399@naver.com>
2020-11-26 19:19:05 +0900

    dayeon's initial commit

---
+++ app/src/main/java/org/techtown/mirimintenttest/activity_result.java
@@ -0,0 +1,14 @@
+package org.techtown.mirimintenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
